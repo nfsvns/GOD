3eb2b61061152beae8827a2099ee9e4378e3aabb
nfsvns <nguyenpham242003@gmail.com>
2024-04-08 16:40:12 +0700

    update

--- src/main/java/com/poly/config/AuthConfig.java
+++ src/main/java/com/poly/config/AuthConfig.java
@@ -19,13 +19,10 @@ import com.poly.service.UserService;
 @EnableWebSecurity
 public class AuthConfig extends WebSecurityConfigurerAdapter {
 
-
 	@Bean
 	public BCryptPasswordEncoder getPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
-
 
 	@Autowired
 	UserService userService;
@@ -46,15 +43,16 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
 		// demo 1
 		http.authorizeRequests()
 
-//		.antMatchers("/history", "/revenue", "/account.html", "/orderDetail", "/productAdmin",
-//			"/categoryAdmin", "/report").hasAnyRole("DIRE", "STAF")
-		.antMatchers("/assets/**").hasAnyRole("STAF", "ADMI")
-		.antMatchers("/check/**").authenticated()
-		.antMatchers("/shop.html/addComments/**").authenticated()
-		.antMatchers("/shop.html/replyComments**").authenticated()
-		
-		.antMatchers("/rest/authorities","/rest/revenue").hasRole("ADMI")
-		.anyRequest().permitAll(); // anonymous
+				// .antMatchers("/history", "/revenue", "/account.html", "/orderDetail",
+				// "/productAdmin",
+				// "/categoryAdmin", "/report").hasAnyRole("DIRE", "STAF")
+				.antMatchers("/assets/**").hasAnyRole("STAF", "ADMI")
+				.antMatchers("/check/**").authenticated()
+				.antMatchers("/shop.html/addComments/**").authenticated()
+				.antMatchers("/shop.html/replyComments**").authenticated()
+
+				.antMatchers("/rest/authorities", "/rest/revenue").hasRole("ADMI")
+				.anyRequest().permitAll(); // anonymous
 
 		// giao diện đăng nhập
 		http.formLogin().loginPage("/login")
@@ -69,12 +67,12 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
 		http.logout().logoutUrl("/logout").logoutSuccessUrl("/logout/success");
 
 		// oauth2 - đăng nhập từ mạng xh
-		http.oauth2Login()
-		.loginPage("/login.html")
-		.defaultSuccessUrl("/oauth2/login/success", true)
-		.failureUrl("/login.html/error")
-		.authorizationEndpoint()
-		.baseUri("/oauth2/authorization");
+		// http.oauth2Login()
+		// .loginPage("/login.html")
+		// .defaultSuccessUrl("/oauth2/login/success", true)
+		// .failureUrl("/login.html/error")
+		// .authorizationEndpoint()
+		// .baseUri("/oauth2/authorization");
 
 	}
 
