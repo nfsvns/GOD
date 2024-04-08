b5adb44c67b5910cdb5b7da29521d2d99b8d1e24
nfsvns <nguyenpham242003@gmail.com>
2024-04-08 18:32:33 +0700

    change

--- src/main/java/com/poly/service/ParamService.java
+++ src/main/java/com/poly/service/ParamService.java
@@ -13,32 +13,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import lombok.val;
-import lombok.experimental.var;
-
 @Service
 public class ParamService {
 	@Autowired
 	HttpServletRequest request;
 	@Autowired
 	ServletContext context;
-	
-	public String getString (String name, String defaulValue) {
+
+	public String getString(String name, String defaulValue) {
 		String result = request.getParameter(name);
 		return result != null ? result : defaulValue;
 	}
-	public int getInt (String name, int defaulValue) {
+
+	public int getInt(String name, int defaulValue) {
 		String result = request.getParameter(name);
 		return result != null ? Integer.parseInt(result) : defaulValue;
 	}
-	public double getDouble (String name, double defaulValue) {
+
+	public double getDouble(String name, double defaulValue) {
 		String result = request.getParameter(name);
 		return result != null ? Double.parseDouble(result) : defaulValue;
 	}
-	public boolean getBoolean (String name, boolean defaulValue) {
+
+	public boolean getBoolean(String name, boolean defaulValue) {
 		String result = request.getParameter(name);
 		return result != null ? Boolean.parseBoolean(result) : defaulValue;
 	}
+
 	public Date getDate(String name, String pattern) {
 		try {
 			SimpleDateFormat formater = new SimpleDateFormat(pattern);
@@ -48,6 +49,7 @@ public class ParamService {
 			return null;
 		}
 	}
+
 	public File save(MultipartFile file, String path) throws Exception {
 		File dir = new File(context.getRealPath(path));
 		if (!dir.exists())
